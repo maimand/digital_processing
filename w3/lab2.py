92e0efad9c96c8d7b45db7065a30c3d2768397c4
Mai Huu Man <manmaihuu@gmail.com>
2020-09-21 12:50:30 +0700

    add lab2

---
+++ w3/lab2.py
@@ -0,0 +1,26 @@
+import numpy as np
+from scipy.io.wavfile import write, read
+import matplotlib.pyplot as plt
+
+samplerate, data = read('studio_male.wav')  
+# print(len(data))
+new_data1 = []
+for i in range(len(data)):
+    if(i%2 == 0):
+        new_data1.append(data[i])
+new_data1 = np.asarray(new_data1)
+# print(len(new_data1))
+samplerate1 = int(samplerate/2)
+
+new_data2 = []
+for i in range(len(data)):
+    if(i%4 == 0):
+        new_data2.append(data[i])
+new_data2 = np.asarray(new_data2)
+# print(len(new_data2))
+samplerate2 = int(samplerate/4)
+
+
+
+write("lab2_1.wav",samplerate1,new_data1)
+write("lab2_2.wav",samplerate2,new_data2)
