f32cb89bcf7daa0b448da52cf154113390f4d6ea
Mai Huu Man <manmaihuu@gmail.com>
2020-09-22 08:12:43 +0700

    add comment for lab 2

--- w3/lab1.py
+++ w3/lab1.py
@@ -2,10 +2,6 @@ import numpy as np
 from scipy.io.wavfile import write
 import matplotlib.pyplot as plt
 
-# fs = 10000
-# samplerate1 = 3*fs
-# samplerate2 = int(1.5*fs)
-
 n1 = np.linspace(-20, 20, 41)
 n2 = np.linspace(-10, 20, 31)
 
@@ -16,9 +12,9 @@ data2 =  np.cos(0.1 * np.pi * n2 - np.pi/5)
 
 fig, axs = plt.subplots(2)
 axs[0].stem(n1, data1)
-axs[0].set_title('cos 1')
+axs[0].set_title('cos1')
 axs[1].stem(n2, data2)
-axs[1].set_title('cos 2')
+axs[1].set_title('cos2')
 
 plt.show()
 
--- w3/lab2.py
+++ w3/lab2.py
@@ -3,21 +3,21 @@ from scipy.io.wavfile import write, read
 import matplotlib.pyplot as plt
 
 samplerate, data = read('studio_male.wav')  
-# print(len(data))
+#data for f/2
 new_data1 = []
 for i in range(len(data)):
     if(i%2 == 0):
         new_data1.append(data[i])
 new_data1 = np.asarray(new_data1)
-# print(len(new_data1))
+#samplerate for f/2
 samplerate1 = int(samplerate/2)
-
+#data for f/4
 new_data2 = []
 for i in range(len(data)):
     if(i%4 == 0):
         new_data2.append(data[i])
 new_data2 = np.asarray(new_data2)
-# print(len(new_data2))
+#samplerate for f/4
 samplerate2 = int(samplerate/4)
 
 
