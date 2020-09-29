4c9721c8dca13ced56770de7e4d4193dcf628c82
Mai Huu Man <manmaihuu@gmail.com>
2020-09-29 09:43:46 +0700

    fix the no causality

--- w4/lab3.py
+++ w4/lab3.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import signal
 from matplotlib import pyplot as plt
+
 n = 51
 
 t = np.linspace(-1., 1., n)
@@ -16,11 +17,11 @@ for i in range(len(a)):
 
 temp2 = np.zeros_like(a)
 for i in range(len(a)):
-    temp2[i] = a[i]
-    for j in range(2):
-        if ((i-j) >= 0):
+    temp2[i] = a[i]*0.2
+    for j in range(1,3):
+        if (i-j) >= 0:
             temp2[i] += a[i-j] * 0.2
-        if ((i+j) < len(a)):
+        if (i+j) < len(a):
             temp2[i] += a[i+j] * 0.2
 # plt.plot(t, a[0], 'r-', t , original, 'b-', t, temp1[0] , 'y-', t, temp2[0], 'o-')
 plt.plot(t , original,'r-',label='original')
