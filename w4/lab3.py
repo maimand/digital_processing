2ed5f8c2d8ab0d827549f8e37498910c55c943ab
Mai Huu Man <manmaihuu@gmail.com>
2020-09-29 09:16:26 +0700

    add a DIY conv and filter

---
+++ w4/lab3.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy import signal
+from matplotlib import pyplot as plt
+n = 51
+
+t = np.linspace(-1., 1., n)
+original = np.sin(2. * np.pi * t)
+a =  np.sin(2. * np.pi * t) + np.random.rand(1, n)
+
+
+temp1 = np.zeros_like(a)
+for i in range(len(a)):
+    for j in range(5):
+        if (i-j) >= 0:
+            temp1[i] += a[i-j] * 0.2
+
+temp2 = np.zeros_like(a)
+for i in range(len(a)):
+    temp2[i] = a[i]
+    for j in range(2):
+        if ((i-j) >= 0):
+            temp2[i] += a[i-j] * 0.2
+        if ((i+j) < len(a)):
+            temp2[i] += a[i+j] * 0.2
+# plt.plot(t, a[0], 'r-', t , original, 'b-', t, temp1[0] , 'y-', t, temp2[0], 'o-')
+plt.plot(t , original,'r-',label='original')
+plt.plot(t, a[0],'b-',label='noised')
+plt.plot(t, temp1[0] , 'y-',label='filter with causality')
+plt.plot(t, temp2[0], 'g-',label='filter with no causality')
+plt.legend()
+plt.show()
